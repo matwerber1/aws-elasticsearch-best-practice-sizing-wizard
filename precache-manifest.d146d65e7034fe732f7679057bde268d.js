a6b571ab3cf4a468cf5bd0dd284ea72f87b606d8
Mat Werber <werberm@amazon.com>
2020-01-22 21:49:50 -0800

    Updates

---
+++ precache-manifest.d146d65e7034fe732f7679057bde268d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40215fd7c402d90dbe66d123711e3b6",
+    "url": "/aws-elasticsearch-best-practice-sizing-wizard/index.html"
+  },
+  {
+    "revision": "22a583f6c71de5e1c085",
+    "url": "/aws-elasticsearch-best-practice-sizing-wizard/static/css/main.2887712e.chunk.css"
+  },
+  {
+    "revision": "6b80a6d5cb1fe4f95f27",
+    "url": "/aws-elasticsearch-best-practice-sizing-wizard/static/js/2.7fd4b9a3.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/aws-elasticsearch-best-practice-sizing-wizard/static/js/2.7fd4b9a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22a583f6c71de5e1c085",
+    "url": "/aws-elasticsearch-best-practice-sizing-wizard/static/js/main.c6fe9254.chunk.js"
+  },
+  {
+    "revision": "9565387a74969c528086",
+    "url": "/aws-elasticsearch-best-practice-sizing-wizard/static/js/runtime-main.05052241.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aws-elasticsearch-best-practice-sizing-wizard/static/media/logo.5d5d9eef.svg"
+  }
+]);
